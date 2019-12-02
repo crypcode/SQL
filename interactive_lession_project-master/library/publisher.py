d3eb49e57a37793d08e54eec927e7803766bf34c
crypcode <55318332+crypcode@users.noreply.github.com>
2019-12-02 12:35:29 +0200

    Naujausia db

---
+++ interactive_lession_project-master/library/publisher.py
@@ -0,0 +1,9 @@
+class publisher:
+
+    def __init__(self, publisher_id, publisher_name, book_name, author, printing_quantity, printing_price):
+        self.publisher_id = publisher_id
+        self.publisher_name = publisher_name
+        self.book_name = book_name
+        self.author = author
+        self.printing_quantity = printing_quantity
+        self.printing_price = printing_price
