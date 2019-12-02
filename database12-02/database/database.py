894275a4e05288752317e1726eb533e627e8dc78
crypcode <55318332+crypcode@users.noreply.github.com>
2019-12-02 15:03:51 +0200

    Optimizuota

---
+++ database12-02/database/database.py
@@ -0,0 +1,323 @@
+import sqlite3
+from library.book import book
+from library.publisher import publisher
+
+
+def open_connection():
+    connection = sqlite3.connect("library.db")
+    cursor = connection.cursor()
+    return connection, cursor
+
+
+def close_connection(connection, cursor):
+    cursor.close()
+    connection.close()
+
+
+# Metodas queriams rasyti sutrumpintas
+def query_database(query, params):
+    try:
+        connection, cursor = open_connection()
+        cursor.execute(query, params)
+        connection.commit()
+        data = cursor.fetchall()
+        print(data)
+
+
+    except sqlite3.DatabaseError as error:
+        print(error)
+    finally:
+        connection.close()
+
+
+def create_book(book):
+    query = """INSERT INTO books VALUES (? ,?, ?, ?)"""
+    params = (book.book_id, book.book_name, book.author, book.selling_price)
+    query_database(query, params)
+
+book1 = book(None, "Tu galfsdfsi", "Josef Murphy", 25.05)
+# create_book(book1)
+
+
+def get_book(book):
+    query = """SELECT * FROM books WHERE book_id = (?) OR book_name = (?) OR author = (?) OR selling_price = (?)"""
+    params = (book.book_id, book.book_name, book.author, book.selling_price)
+    query_database(query, params)
+
+get_book(book1)
+
+
+def update_book(book):
+    query = """UPDATE books SET book_name = 'Belekas' WHERE book_name = (?) OR book_id = (?) OR author = (?) OR 
+        selling_price = (?) """
+    params = (book.book_name, book.book_id, book.author, book.selling_price)
+    query_database(query, params)
+
+
+def delete_book(book):
+    query = """DELETE FROM books WHERE book_name = (?) OR book_id = (?) OR author = (?) OR selling_price = (?)"""
+    params = (book.book_name, book.book_id, book.author, book.selling_price)
+    query_database(query, params)
+
+
+# ------------------------------------------------------------------------------------
+
+# Senas metodas virsuje sutrumpintas
+
+# def create_books_table():
+#     try:
+#         connection, cursor = open_connection()
+#         query = """CREATE TABLE IF NOT EXISTS books (
+#                     book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#                     book_name TEXT UNIQUE,
+#                     author TEXT,
+#                     selling_price REAL)
+#                 """
+#
+#         cursor.execute(query)
+#
+#     except sqlite3.DatabaseError as error:
+#         print(error)
+#
+#     finally:
+#         close_connection(connection, cursor)
+#
+#
+# def create_publisher_table():
+#     try:
+#         connection, cursor = open_connection()
+#         query = """CREATE TABLE IF NOT EXISTS publishers (
+#                     publisher_id integer PRIMARY KEY AUTOINCREMENT,
+#                     publisher_name text UNIQUE,
+#                     book_name text UNIQUE,
+#                     author text,
+#                     printing_quantiy integer,
+#                     printing_price integer)
+#                 """
+#
+#         cursor.execute(query)
+#     except sqlite3.DatabaseError as error:
+#         print(error)
+#
+#     finally:
+#         close_connection(connection, cursor)
+#
+#
+# create_books_table()
+# create_publisher_table()
+#
+#
+# def function(object):
+#     try:
+#         connection, cursor = open_connection()
+#
+#         query = "INSERT INTO "
+#         table
+#         " VALUES (?,?,?)"
+#
+#         query_parameters = (object.title, object.year)
+#
+#         cursor.execute(query, query_parameters)
+#
+#         connection.commit()
+#
+#     except sqlite3.DatabaseError as error:
+#         print(error)
+#
+#     finally:
+#         close_connection(connection, cursor)
+#
+#
+# def create_book(book):
+#     try:
+#         connection, cursor = open_connection()
+#
+#         query = "INSERT INTO books VALUES (? ,?, ?, ?)"
+#         query_parameters = (book.id, book.book_title, book.author, book.selling_price)
+#         cursor.execute(query, query_parameters)
+#         connection.commit()
+#
+#     except sqlite3.DataError as error:
+#         print(error)
+#
+#     finally:
+#         close_connection(connection, cursor)
+#
+#
+# book1 = book(None, "Tu gali", "Josef Murphy", 25.05)
+#
+#
+# # create_book(book1)
+#
+# def get_book(book):
+#     try:
+#         connection, cursor = open_connection()
+#
+#         query = "SELECT * FROM books WHERE book_id = (?) OR book_name = (?) OR author = (?) OR selling_price = (?)"
+#
+#         query_parameters = (book.book_id, book.book_name, book.author, book.selling_price)
+#
+#         for row in cursor.execute(query, query_parameters):
+#             print(row)
+#
+#         connection.commit()
+#
+#     except sqlite3.DataError as error:
+#         print(error)
+#
+#     finally:
+#         close_connection(connection, cursor)
+#
+#
+# get_book(book1)
+#
+#
+# def update_book(book):
+#     try:
+#         connection, cursor = open_connection()
+#
+#         query = "UPDATE books SET book_name = 'Belekas' WHERE book_name = (?) OR book_id = (?) OR author = (?) OR selling_price = (?)"
+#
+#         query_parameters = (book.book_name, book.book_id, book.author, book.selling_price)
+#
+#         cursor.execute(query, query_parameters)
+#
+#         connection.commit()
+#
+#     except sqlite3.DataError as error:
+#         print(error)
+#
+#     finally:
+#         close_connection(connection, cursor)
+#
+#
+# update_book(book1)
+# get_book(book1)
+#
+#
+# def delete_book(book):
+#     try:
+#         connection, cursor = open_connection()
+#
+#         query = "DELETE FROM books WHERE book_name = (?) OR book_id = (?) OR author = (?) OR selling_price = (?)"
+#
+#         query_parameters = (book.book_name, book.book_id, book.author, book.selling_price)
+#
+#         cursor.execute(query, query_parameters)
+#
+#         connection.commit()
+#     except sqlite3.DataError as error:
+#         print(error)
+#     finally:
+#         close_connection(connection, cursor)
+#
+#
+# delete_book(book1)
+# get_book(book1)
+#
+#
+# def create_publisher(publisher):
+#     try:
+#         connection, cursor = open_connection()
+#         query = "INSERT INTO publishers VALUES (?,?,?,?,?,?)"
+#         parameters = (publisher.publisher_id, publisher.publisher_name, publisher.book_name,
+#                       publisher.author, publisher.printing_quantity, publisher.printing_price)
+#         cursor.execute(query, parameters)
+#         connection.commit()
+#     except sqlite3.DataError as error:
+#         print(error)
+#     finally:
+#         close_connection(connection, cursor)
+#
+#
+# publisher1 = publisher(None, "Baltos_lankos", "Tu_gali", "Josef_Murphy", 5000, 8)
+#
+#
+# # create_publisher(publisher1)
+#
+#
+# def get_publisher(publisher):
+#     try:
+#         connection, cursor = open_connection()
+#         query = """SELECT * FROM publishers WHERE publisher_id = (?) OR publisher_name = (?) OR book_name = (?) OR author = (?) OR
+#                     printing_quantiy = (?) OR printing_price = (?)"""
+#         parameters = (publisher.publisher_id, publisher.publisher_name, publisher.book_name,
+#                       publisher.author, publisher.printing_quantity, publisher.printing_price)
+#
+#         for row in cursor.execute(query, parameters):
+#             print(row)
+#
+#     except sqlite3.DataError as error:
+#         print(error)
+#     finally:
+#         close_connection(connection, cursor)
+#
+#
+# # get_publisher(publisher1)
+#
+#
+# def update_publisher(publisher):
+#     try:
+#         connection, cursor = open_connection()
+#         query = """UPDATE publishers set publisher_name = 'zalios_lankos' WHERE publisher_id = (?) OR
+#                     publisher_name = (?) OR book_name = (?) OR author = (?) OR
+#                     printing_quantiy = (?) OR printing_price = (?) """
+#         parameters = (publisher.publisher_id, publisher.publisher_name, publisher.book_name,
+#                       publisher.author, publisher.printing_quantity, publisher.printing_price)
+#         cursor.execute(query, parameters)
+#         connection.commit()
+#     except sqlite3.DataError as error:
+#         print(error)
+#     finally:
+#         close_connection(connection, cursor)
+#
+#
+# # update_publisher(publisher1)
+# # get_publisher(publisher1)
+#
+#
+# def delete_publisher(publisher):
+#     try:
+#         connection, cursor = open_connection()
+#         query = """DELETE FROM publishers WHERE publisher_id = (?) OR
+#                     publisher_name = (?) OR book_name = (?) OR author = (?) OR
+#                     printing_quantiy = (?) OR printing_price = (?) """
+#         parameters = (publisher.publisher_id, publisher.publisher_name, publisher.book_name,
+#                       publisher.author, publisher.printing_quantity, publisher.printing_price)
+#         cursor.execute(query, parameters)
+#         connection.commit()
+#     except sqlite3.DataError as error:
+#         print(error)
+#     finally:
+#         close_connection(connection, cursor)
+#
+#
+# # delete_publisher(publisher1)
+# # get_publisher(publisher1)
+#
+# # filter by single field
+# def filter_by_name(publisher):
+#     book_name = publisher.book_name
+#     return book_name
+#
+#
+# def filter_by_field(publisher):
+#     try:
+#         publisher_name = filter_by_name(publisher)
+#         connection, cursor = open_connection()
+#
+#         query = """SELECT * FROM publishers WHERE book_name = (?)"""
+#
+#         parameters = [publisher_name]
+#         parameters_comperhesion = [paramter for paramter in parameters]
+#
+#         for i in cursor.execute(query, parameters_comperhesion):
+#             print(i)
+#
+#     except sqlite3.DataError as error:
+#         print(error)
+#     finally:
+#         close_connection(connection, cursor)
+#
+#
+# filter_by_field(publisher1)
---
+++ database12-02/library/book.py
@@ -0,0 +1,7 @@
+class book:
+
+    def __init__(self, book_id, book_name, author, selling_price):
+        self.book_id = book_id
+        self.book_name = book_name
+        self.author = author
+        self.selling_price = selling_price
